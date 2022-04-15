be493b966e2887aed8140dc29dcd4204e79dc8ab
amaurydelaroque <amaury.delaroque@oclock.io>
2022-04-15 11:58:58 +0200

    adding storybook and sass-loader + storybook configuration working

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,31 +2,15 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2020,
-    sourceType: 'module',
+    sourceType: 'module'
   },
   // TODO AirBnB config
-
   settings: {
     react: {
-      version: 'detect',
-    },
+      version: 'detect'
+    }
   },
-  extends: [
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:import/typescript',
-    'plugin:jsx-a11y/recommended',
-    'plugin:eslint-comments/recommended',
-    'prettier',
-    'prettier/@typescript-eslint',
-    'plugin:prettier/recommended',
-    'plugin:jest-dom/recommended',
-  ],
+  extends: ['plugin:react/recommended', 'plugin:react-hooks/recommended', 'plugin:@typescript-eslint/recommended', 'eslint:recommended', 'plugin:@typescript-eslint/eslint-recommended', 'plugin:import/errors', 'plugin:import/warnings', 'plugin:import/typescript', 'plugin:jsx-a11y/recommended', 'plugin:eslint-comments/recommended', 'prettier', 'prettier/@typescript-eslint', 'plugin:prettier/recommended', 'plugin:jest-dom/recommended', 'plugin:storybook/recommended'],
   rules: {
     'no-unused-vars': 'off',
     '@typescript-eslint/no-unused-vars': ['error'],
@@ -39,6 +23,6 @@ module.exports = {
     // check supported rules for jest-dom https://github.com/testing-library/eslint-plugin-jest-dom#supported-rules
     'jest-dom/prefer-checked': 'error',
     'jest-dom/prefer-enabled-disabled': 'error',
-    'jest-dom/prefer-to-have-attribute': 'error',
-  },
+    'jest-dom/prefer-to-have-attribute': 'error'
+  }
 };
